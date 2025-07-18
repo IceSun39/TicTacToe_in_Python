c8928ce0669e4b735de338ea80b9240f090643e4
Vlad Kolesnyk <kolesnikvlad416@.com>
2025-07-18 16:21:57 +0300

    Added CellState and board

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Welcome to Tic Tac Toe")
+from enum import Enum
+
+class CellState(Enum):
+    EMPTY = " "
+    X = "X"
+    O = "O"
+
+    def is_empty(self):
+        return self == CellState.EMPTY
